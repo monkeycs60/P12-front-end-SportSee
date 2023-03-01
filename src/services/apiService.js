ffaaf53b82cd241d6ab46d55039f93dbeabbf9da
Clement SERIZAY <clement.serizay@gmail.com>
2023-03-01 15:24:13 +0100

    4 endpoints api service

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -9,3 +9,18 @@ export async function getDataUser(userId) {
   return response.data;
 }
 
+export async function getDataUserActivity(userId) {
+  const response = await api.get(`/user/${userId}/activity`);
+  return response.data;
+}
+
+export async function getDataUserAverageSession(userId) {
+  const response = await api.get(`/user/${userId}/average-session/`);
+  return response.data;
+}
+
+export async function getDataUserPerformance (userId) {
+  const response = await api.get(`/user/${userId}/performance`);
+  return response.data;
+}
+
