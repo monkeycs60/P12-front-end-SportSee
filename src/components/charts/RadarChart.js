9ceddce2fd7fe2bbac5277296b91b8b0622731fa
monkeycs60 <clement.serizay@gmail.com>
2023-03-17 02:40:59 +0100

    radar chart almost finished

--- src/components/charts/RadarChart.js
+++ src/components/charts/RadarChart.js
@@ -5,6 +5,7 @@ import styled from "styled-components";
 
 const RadarChart = ({ dataUserPerformance }) => {
     const chartRef = useRef(null);
+    const levels = 5;
     
     useEffect(() => {
         if (dataUserPerformance) {
@@ -12,8 +13,8 @@ const RadarChart = ({ dataUserPerformance }) => {
       const chartElement = chartRef.current;
       const chartData = Object.entries(dataUserPerformance);
 
-      const width = 300;
-      const height = 180;
+      const width = 400;
+      const height = 280;
       const margin = { top: 20, right: 20, bottom: 20, left: 20 };
       const radius = Math.min(width, height) / 2;
 
@@ -30,13 +31,14 @@ const RadarChart = ({ dataUserPerformance }) => {
           `translate(${width / 2 + margin.left}, ${height / 2 + margin.top})`
         );
 
-
-      const radarLine = d3
-        .lineRadial()
-        .radius((d) => d.value)
-        .angle((d, i) => i * angleSlice);
-
-      const color = d3.scaleOrdinal().range(['#1f77b4']);
+ // Add grey background
+      svg
+        .append('rect')
+        .attr('x', -(width / 2) - margin.left)
+        .attr('y', -(height / 2) - margin.top)
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .attr('fill', '#282D30');
 
       const maxValue = Math.max(
         ...chartData.map(([, value]) => value),
@@ -44,6 +46,31 @@ const RadarChart = ({ dataUserPerformance }) => {
       );
 
       const yScale = d3.scaleLinear().domain([0, maxValue]).range([0, radius]);
+
+
+        for (let level = 1; level <= levels; level++) {
+        const levelFactor = (radius / levels) * level;
+        const polygonPoints = chartData
+          .map((_, i) => [
+            levelFactor * Math.cos(angleSlice * i - Math.PI / 2),
+            levelFactor * Math.sin(angleSlice * i - Math.PI / 2),
+          ])
+          .join(' ');
+
+        svg
+          .append('polygon')
+          .attr('points', polygonPoints)
+          .attr('stroke', 'white')
+          .attr('stroke-width', '1px')
+          .attr('fill', 'none');
+      }
+
+       const radarLine = d3
+        .lineRadial()
+        .radius((d) => d.value)
+        .angle((d, i) => i * angleSlice);
+
+      const color = d3.scaleOrdinal().range(['#1f77b4']);
 
       const yAxis = svg
         .selectAll('.axis')
@@ -53,43 +80,35 @@ const RadarChart = ({ dataUserPerformance }) => {
         .attr('class', 'axis');
 
       yAxis
-        .append('line')
-        .attr('x1', 0)
-        .attr('y1', 0)
-        .attr('x2', (d, i) => radius * Math.cos(angleSlice * i - Math.PI / 2))
-        .attr('y2', (d, i) => radius * Math.sin(angleSlice * i - Math.PI / 2))
-        .attr('stroke', 'grey')
-        .attr('stroke-width', '1px');
-
-      yAxis
         .append('text')
         .attr(
           'x',
           (d, i) =>
             (radius + 10) * Math.cos(angleSlice * i - Math.PI / 2) -
-            10 * (d[0].length - 1)
+            10 * (d[0].length - 1) + 40
         )
         .attr(
           'y',
           (d, i) => (radius + 10) * Math.sin(angleSlice * i - Math.PI / 2) + 5
         )
         .text((d) => d[0])
-        .style('font-size', '12px')
-        .style('fill', 'grey');
+        .style('font-size', '14px')
+        .style('fill', 'white');
 
-      const path = svg
-        .datum(
+      svg
+        .selectAll('.radar-path')
+        .data([
           chartData.map(([label, value]) => ({
             label,
             value: yScale(value),
-          }))
-        )
+          })),
+        ])
+        .enter()
         .append('path')
+        .attr('class', 'radar-path')
         .attr('d', radarLine)
-        .style('fill', 'red')
-        .style('fill-opacity', 0.7)
-        .style('stroke', 'transparent')
-        .style('stroke-width', '2px');
+        .style('fill', "#FF0000")
+        .style('fill-opacity', 0.6);
 
       return () => {
         d3.select(chartElement).selectAll('*').remove();
