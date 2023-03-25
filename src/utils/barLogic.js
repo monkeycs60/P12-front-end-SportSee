e3d21c801492438ff97145bc3898ab133606a16f
monkeycs60 <clement.serizay@gmail.com>
2023-03-25 20:21:09 +0100

    jsdoc + split logic tooltip bar

--- src/utils/barLogic.js
+++ src/utils/barLogic.js
@@ -1,6 +1,7 @@
 import * as d3 from 'd3';
 import { drawRoundedBar } from './formating/drawRoundedBar';
 import { formatDate } from './formating/formatDate';
+import { handleMouseOver, handleMouseMove, handleMouseOut, formatTooltipContent } from './tooltipCharts/tooltipBar';
 
 /**
 Draws a bar chart with rounded bars and tooltips for data visualization using D3
@@ -17,71 +18,6 @@ Draws a bar chart with rounded bars and tooltips for data visualization using D3
 @returns {void}
 */
 export const barLogic = (dataUserActivity, d3Container, width, height, fontLegend, tickPaded, tickSized, xAxisPos, tooltipRef) => {
-
-  /** 
-Handles the mouseover event of the chart bars.
-@function
-@param {Element} element - The element being hovered over.
-@param {Object} d - The data object of the hovered bar.
-@returns {void}
-*/
- const handleMouseOver = (element, d) => {
-  d3.select(element)
-    .attr("fill", "rgba(128, 128, 128, 0.3)");
-
-  d3.select(tooltipRef.current)
-    .style("display", "block")
-    .style("background-color", "red")
-    .style("padding", "8px")
-    .style("border-radius", "4px")
-    .style("font-size", "12px")
-    .style("color", "white")
-    .html(formatTooltipContent(d));
-};
-
-/**
-Handles the mousemove event of the chart bars.
-@function
-@param {Event} event - The mousemove event.
-@returns {void}
-*/
-const handleMouseMove = (event) => {
-  const tooltipWidth = tooltipRef.current.clientWidth;
-  const tooltipHeight = tooltipRef.current.clientHeight;
-  const offsetX = 80;
-  const offsetY = 40;
-
-  d3.select(tooltipRef.current)
-    .style("left", `${event.pageX - tooltipWidth + offsetX}px`)
-    .style("top", `${event.pageY - tooltipHeight - offsetY}px`);
-};
-
-/**
-Handles the mouseout event of the chart bars.
-@function
-@param {Element} element - The element being hovered over.
-@returns {void}
-*/
-const handleMouseOut = (element) => {
-  d3.select(element)
-    .attr("fill", "transparent");
-
-  d3.select(tooltipRef.current).style("display", "none");
-};
-
-/**
-Formats the content of the tooltip for a given data object.
-@function
-@param {Object} d - The data object for which the tooltip content will be formatted.
-@returns {string} The formatted tooltip content.
-*/
-const formatTooltipContent = (d) => {
-  return `
-    ${d.kilogram}kg<br />
-    <br />
-    ${d.calories}kcal<br />
-  `;
-};
   
         const borderRadius = 3;
  
@@ -203,10 +139,10 @@ svg.selectAll('.y.axis')
   .attr("height", height)
   .attr("fill", "transparent")
   .on("mouseover", function (event, d) {
-    handleMouseOver(this, d);
+    handleMouseOver(this, d, tooltipRef);
   })
-  .on("mousemove", handleMouseMove)
+  .on("mousemove", (event) => handleMouseMove(event, tooltipRef))
   .on("mouseout", function () {
-    handleMouseOut(this);
+    handleMouseOut(this, tooltipRef);
   });
 }
